f8145af1e12bcec348f1e59dcee901ecd7f0678b
XBladeXGMD <80782451+XBladeXGMD@users.noreply.github.com>
2024-05-11 20:19:02 -0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -100,25 +100,25 @@ export default {
                     </template>
                     <h3>Submission Requirements</h3>
                     <p>
-                        Achieved the record without using hacks (however, FPS bypass is allowed, up to 360fps)
+                        There is no limit on what FPS You can use. Records can be achieved with the "Click in Between Frames" mod, but not with mods that directly alter physics.
                     </p>
                     <p>
-                        Achieved the record on the level that is listed on the site - please check the level ID before you submit a record
+                        The record must be completed on the original level.
                     </p>
                     <p>
-                        Have either source audio or clicks/taps in the video. Edited audio only does not count
+                        Hard Demons and above require click/tap audio or keystrokes displayed.
                     </p>
                     <p>
-                        The recording must have a previous attempt and entire death animation shown before the completion, unless the completion is on the first attempt. Everyplay records are exempt from this
+                        Cut recordings must show the footage of the entire checkpoint attempt. You can cut the video once you hit another checkpoint.
                     </p>
                     <p>
-                        The recording must also show the player hit the endwall, or the completion will be invalidated.
+                        The recording must have cheat indicator present the entire time (including on end screen) and must show the entire level complete animation.
                     </p>
                     <p>
-                        Do not use secret routes or bug routes
+                        Do not use secret routes or bug routes (Blade quartet quota completion 😬)
                     </p>
                     <p>
-                        Do not use easy modes, only a record of the unmodified level qualifies
+                        All completions require a video.
                     </p>
                     <p>
                         Once a level falls onto the Legacy List, we accept records for it for 24 hours after it falls off, then afterwards we never accept records for said level
